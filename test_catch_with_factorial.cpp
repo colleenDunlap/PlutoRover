841b19c6434c5a517a59258744b49ba9404b781f
Colleen Dunlap <colleen.dunlap@marquette.edu>
2018-12-14 19:13:04 +0000

    further initialization tests, currently failing #TDDlife

--- test_catch_with_factorial.cpp
+++ test_catch_with_factorial.cpp
@@ -4,8 +4,23 @@
 
 TEST_CASE( "Rover is Initialized", "[initializeRover]" ) {
     PlutoRover* rover = new PlutoRover();
+    
     REQUIRE(rover->xPos == 0);
     REQUIRE(rover->yPos == 0);
     REQUIRE(rover->Direction == 'N');
+    
+    SECTION("Initialize Rover with Parameters"){
+        PlutoRover* rover = new PlutoRover(3, 5, 'S');
+        REQUIRE(rover->xPos == 3);
+        REQUIRE(rover->yPos == 5);
+        REQUIRE(rover->Direction=='S');
+    }
+    SECTION("Initialize Rover with Parameters Outside Planet Bounds"){
+        PlutoRover* rover = new PlutoRover(200, 250, 'W');
+        
+        REQUIRE(rover->xPos == 0);
+        REQUIRE(rover->yPos == 50);
+        REQUIRE(rover->Direction=='W');
+    }
 
 }
